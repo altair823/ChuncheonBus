3e4b43dff3ef1db6a67dfa256176cbccb072323d
altair823 <dlsrks0734@gmail.com>
2021-06-22 06:38:00 +0900

    Add scraper and string extractor to lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,2 @@
-# From the maintenance point of view, dynamic definition of __all__ is more flexible when the package is small.
+# From the view point of maintenance, dynamic definition of __all__ is more flexible when the package is small.
 # __all__ = ["Scraper", "StringExtractor"]
