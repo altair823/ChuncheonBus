b920cd380a8766073ad84ce4ea88247bb925f61e
altair823 <dlsrks0734@gmail.com>
2021-06-23 01:29:23 +0900

    Add string processing

--- lib/string_extractor.py
+++ lib/string_extractor.py
@@ -1,35 +1,54 @@
 # Higher level abstract module that handling html string.
 # Extract meaningful strings from html string that scraped by scraper module.
 
-def get_extractor(html_string):
+# Factory that construct StringExtractor.
+def get_extractor(html_string=None):
     return StringExtractor(html_string)
 
 
 class StringExtractor:
 
-    def __init__(self, html_string):
+    # Constructor
+    def __init__(self, html_string=None):
+        if html_string is None:
+            raise AttributeError("There is no html String!")
         self.total_html_string = html_string
-        self.target = None
+        self.target_start = None
+        self.target_end = None
 
-    def set_target(self, target_string):
+    # Set target starting string.
+    def set_start_target(self, target_string):
         if target_string == "":
             print("Target string for finding is empty!")
             print("Please check the string to find.")
             exit(1)
             # In temporary, absence of target string cause abort program.
             # But for making flexible program, there should be re-inputting the target.
-        self.target = target_string
+        self.target_start = target_string
 
+    # Set target ending string.
+    def set_end_target(self, target_string):
+        if target_string == "":
+            print("Target string for finding is empty!")
+            print("Please check the string to find.")
+            exit(1)
+        self.target_end = target_string
+
+    # Find index of target string in html string.
+    # And store all found index to found_line_set, return it.
     def find_string(self):
-        found_line_set = set()
-        found_index = 0
-        while found_index < len(self.total_html_string):
-            found_index = self.total_html_string.find(self.target, found_index)
-            while self.total_html_string[found_index] != "\n":
-                found_index -= 1
-            found_index += 1
-            found_line_set.add(found_index)
-            print(found_index)
-            found_index += len(self.target)
+        found_line_set = list()
+        index = 0
+        while True:
+            index = self.total_html_string.find(self.target_start, index)
+            if index == -1:
+                break
+            index += len(self.target_start)
+            first_index = index
+            index = self.total_html_string.find(self.target_end, index)
+            last_index = index
+            found_line_set.append((first_index, last_index))
+            print(index)
+            index = last_index+1
 
         return found_line_set
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import requests.exceptions
 
+import lib.string_extractor
 from lib import scraper
 from lib import string_extractor
 
@@ -17,15 +18,25 @@ class CCBusScraper:
         self.header = _scraper.get_header()
         self.html_string = _scraper.get_html_string()
 
+        # the set of bus timetables.
+        self.bus_list = set()
+
     def get_total_string(self):
         return self.html_string
 
-    def find_buses(self):
-        _string_extractor = string_extractor.StringExtractor(self.html_string)
-        _string_extractor.set_target("<TD>")
+    def _find_buses_from_html(self):
+        _string_extractor = lib.string_extractor.get_extractor(self.html_string)
+        _string_extractor.set_start_target("<TD>")
+        _string_extractor.set_end_target("</TD>")
         return _string_extractor.find_string()
+
+    def find_buses(self):
+        self.bus_list = self._find_buses_from_html()
+        for i in self.bus_list:
+            print(self.html_string[i[0]:i[1]])
 
 
 a = CCBusScraper("http://www.chuncheon-pti.kr/index.php?mp=p2_4_1")
 #print(a.get_total_string())
-print(a.find_buses())
+#print(a.find_buses())
+a.find_buses()
