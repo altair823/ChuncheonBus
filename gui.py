a330829b4b5707f5f97c418d4d1826f8a1c2cb8d
altair823 <dlsrks0734@gmail.com>
2021-06-25 00:51:33 +0900

    release: release of Chuncheon Bus timetable scraper
    second release of scraper program.
    
    Resolve: #2

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 import sys
+import os
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("lib/bus_gui.ui")[0]
+form_class = uic.loadUiType(os.path.abspath("bus_gui.ui"))[0]
 
 #화면을 띄우는데 사용되는 Class 선언
 class BusGUI(QMainWindow, form_class) :
